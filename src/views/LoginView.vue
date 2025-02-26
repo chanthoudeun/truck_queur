22e1e48516192cd39ef6eddba3e2ba261647c617
Chanthou DEUN <chanthou.deun@chipmong.com>
2025-02-26 17:58:15 +0700

    add background

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import apis from '@/libraries/apis';
-import { ILoginReq } from '@/libraries/model/auth/iLoginReq';
-import router from '@/router';
-import { useAuthStore } from '@/stores/auth';
-import { createPinia } from 'pinia';
-import { nextTick, ref } from 'vue';
+import apis from "@/libraries/apis";
+import { ILoginReq } from "@/libraries/model/auth/iLoginReq";
+import router from "@/router";
+import { useAuthStore } from "@/stores/auth";
+import { createPinia } from "pinia";
+import { nextTick, ref } from "vue";
 const pinia = createPinia();
-const authStore = useAuthStore(pinia); 
+const authStore = useAuthStore(pinia);
 const loginRequest = ref<ILoginReq>({
   employeeId: "900182033",
   password: "123",
@@ -15,34 +15,46 @@ const loginRequest = ref<ILoginReq>({
 const login = async () => {
   try {
     var rep = await apis.login(loginRequest.value);
-    var result = rep; 
+    var result = rep;
     if (result && result.token) {
       authStore.setUser(result);
       console.log("Login successful:", result);
       console.log("Auth Store successful:", authStore.user?.token);
-      router.push("/dashboard"); 
-      location.reload(); 
+      router.push("/dashboard");
+      location.reload();
     } else {
       alert(`Login failed! Please check your credentials. ${result}`);
-    
     }
-
   } catch (error) {
     console.error("Login error:", error);
     alert("An error occurred during login.");
   }
 };
-
-
 </script>
-<template >
-    <div>
+<template>
+  <div class="bg-login">
+    <div class="d-flex justify-content-center align-items-center">
+      <div class="row align-items-center">
+        <div class="col-6">Column</div>
+        <div class="col-6">
+          Column
+          <div class="col-6">
+            Column
+            <div class="col-6">Column</div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <h1>Login Page</h1>
     <button @click="login">Login</button>
   </div>
 </template>
 
-
-<style lang="">
-    
+<style>
+.bg-login {
+  background-image: url("@/assets/image/bg-login-web.png");
+  height: 100vh;
+}
 </style>
--- src/views/RegisterTruckView.vue
+++ src/views/RegisterTruckView.vue
@@ -2,6 +2,14 @@
     <div>
         
     </div>
+
+    <div>
+       <div>
+        <div>
+        
+    </div>
+       </div>
+    </div>
 </template>
 <script setup lang="ts">
 
